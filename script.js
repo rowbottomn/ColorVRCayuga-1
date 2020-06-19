23f6286e8819c90fbd94effb4fc8ef8e0f903a6b
Nathan Rowbottom <nathan.rowbottom@snpsteam.com>
2020-06-19 03:47:03 +0000

    Added color indicator which changes colors randomly.  Fixed collider.

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ var randInt = (max, min) =>{
   return Math.floor(randDbl(max,min));
 }
 
+
 var spawnGrid = (primitive)=>{
   var halfCol = Math.floor(numCol/2); 
   for (var y = 0; y < numRow; y++ ){
@@ -64,11 +65,12 @@ var spawnGrid = (primitive)=>{
       newEntity.setAttribute("position",x+" "+(2*y+3.6)+" -5" );
       newEntity.setAttribute("scale","0.999 0.999 0.999" );
       temp =  randInt(colorArray.length, 0);
+      
       const tempCol = colorArray[temp];
      // log(tempCol);
       newEntity.classList.add("target");
       //newEntity.setAttribute("class", "target");
-      
+      newEntity.setAttribute("colorIndex", temp);
       newEntity.setAttribute("material", "color:"+tempCol);
      // newEntity.setAttribute("material", "transparent : true; opacity : 0.9");
       newEntity.setAttribute("sound", "src: #"+tempCol+"-cay; poolSize : 5; on: click");
@@ -78,7 +80,7 @@ var spawnGrid = (primitive)=>{
       //registring a custom component
       newEntity.setAttribute("mute-vel", "el."+newEntity);
       newEntity.addEventListener('click', function (evt) {
-      console.log('Myclass is : ', evt.detail.el, "and my color is: ", this);
+      //console.log('Myclass is : ', evt.detail.el, "and my color is: ", this);
     });
       gridArray[x,y] = newEntity;
       scene.appendChild(newEntity);
@@ -101,7 +103,12 @@ var shoot = () => {
   let pos = myCamera.getAttribute("position");
   //set the bullet position to the player position
   bullet.setAttribute("position", pos);
-
+  bullet.setAttribute("scale", "0.3 0.3 0.3");
+  var temp = indicator.getAttribute("colorIndex");
+  var tempCol = indicator.getAttribute("color");
+  bullet.setAttribute("colorIndex", temp);
+  bullet.setAttribute("material", "color:"+tempCol);
+     // newEntity.setAttribute("material", "transparent : true; opacity : 0.9");
   //set the bullet velocity to the camera direction a
 
 //  bullet.setAttribute("velocity", getDirection( bulletSpeed));
@@ -110,18 +117,19 @@ var shoot = () => {
   console.log(shootDirection);
   shootDirection.multiplyScalar(bulletSpeed);
   //setting the physics properties of the bullet
-  bullet.setAttribute("dynamic-body", "mass:1.0");
+  bullet.setAttribute("dynamic-body", "mass:1.0; shape : sphere; sphereRadius : 0.15");
   
   
-  bullet.setAttribute("radius", 0.5);
-  bullet.setAttribute("emissive", "#EEFF11");
+ // bullet.setAttribute("radius", 0.5);
+  bullet.setAttribute("emissive", tempCol);
   //set the texture
   //bullet.setAttribute("src", "/assets/orb.png");
   //  bullet.setAttribute("material", "emissive = #EEFF11; emissiveIntensity = 1; color = #FFFF00");
   //actutally at the a-entitiy to the a-scene
   scene.appendChild(bullet);
-
+ changeIndicator();
   bullet.setAttribute("velocity", shootDirection);
+  bullet.setAttribute("bounces", "0");
   //add addEventListener for the collision
   bullet.addEventListener('collide', shootCollided);
 };
@@ -130,10 +138,15 @@ const shootCollided = event => {
 
   //check the id of whatever you hit
   if (event.detail.body.el.id === 'floor') {
-    console.log('Hit the floor');
     
+    var numBounces = parseInt(event.detail.target.el.getAttribute("bounces"))+1;
+    console.log('Hit the floor', event.detail.target.el.getAttribute("bounces"));
+    if (numBounces <3  ){
+        event.detail.target.el.setAttribute("bounces", ""+numBounces);
+    }
+    else {
     event.detail.target.el.removeEventListener('collide', shootCollided);
-    scene.removeChild(event.detail.target.el);
+    scene.removeChild(event.detail.target.el);}
   } else if (event.detail.body.el.className === 'target') {
     console.log('Hit the target!');
     event.detail.target.el.removeEventListener('collide', shootCollided);
@@ -150,9 +163,19 @@ document.onkeyup = event => {
   if (event.which == 32) {
     shoot();
   } else if (event.which == 67) {
-    spawnEntities();
+    //spawnEntities();
   }
 };
+
+var changeIndicator = ()=>{
+  var temp =  randInt(colorArray.length, 0);
+  indicator.setAttribute("colorIndex", temp);
+  var tempCol = colorArray[temp];
+  indicator.setAttribute("colorIndex", temp );
+  indicator.setAttribute("color", tempCol);
+  indicator.setAttribute("sound", "src: #"+tempCol+"-cay; poolSize : 5; on: click");
+
+}
 
 var indicator = document.getElementById("indicator");
 
@@ -172,8 +195,8 @@ const maxEntities = 10;
 var myCamera = document.getElementById('camera');
 var cursor;
 
-var numRow = 4;
-var numCol = 10;
+var numRow = 3;
+var numCol = 6;
 var gridArray = [,];
 
 //set the intial level
@@ -182,3 +205,4 @@ var shootDirection = new THREE.Vector3();
 const bulletSpeed = -20;
 
 spawnGrid("a-box");
+changeIndicator();
